46b7f6f9bc0a2e71cff9ac9c10e4a837c54d806f
bradymiller <bradymiller@users.sourceforge.net>
2015-09-08 21:11:09 -0700

    some more fixes

--- library/classes/rulesets/Amc/reports/AMC_304h_STG2/Numerator.php
+++ library/classes/rulesets/Amc/reports/AMC_304h_STG2/Numerator.php
@@ -34,7 +34,6 @@ class AMC_304h_STG2_Numerator implements AmcFilterIF
 		$amcElement = amcCollect('provide_sum_pat_amc',$patient->id,'form_encounter',$patient->object['encounter']);
 		if (!(empty($amcElement))) {
 		  $daysDifference = businessDaysDifference( date("Y-m-d",strtotime($patient->object['date'])) , date("Y-m-d",strtotime($amcElement['date_completed'])) );
-		  error_log("DEBUG: ".$daysDifference,0);
 		  if ($daysDifference < 2) {
 			return true;
 		  }
